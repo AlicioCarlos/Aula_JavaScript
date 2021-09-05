51dcc51d69403ec82f7304d6bff0c30eccfec013
AlicioCarlos <carlosalicio.developer@gmail.com.br>
2021-09-05 14:17:10 -0300

    Atualização

---
+++ js/main.js
@@ -0,0 +1,121 @@
+
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por Clicar!</b>";
+    // console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar(){
+    window.open("https://digitalinnovation.one/");
+    // window.location.href = "https://digitalinnovation.one/"
+}
+
+function trocar(elemento){
+    //document.getElementById("mouseOver").innerHTML = "Obrigado Por Passar o Mouse!";
+    elemento.innerHTML = "Obrigado Por Passar o Mouse!se Aqui!";
+    //alert("Trocar Texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse Aqui!";
+    //alert("Trocar Texto");
+}
+
+function load(){
+    alert("Página Carregada!");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+// alert(soma(5, 10));
+
+// var validar = 0;
+// function validaIdade(idade){
+//     validar;
+//     if(idade >= 18){
+//         validar = true;
+//     }else{
+//         validar = false;
+//     }
+//     return validar
+// }
+
+// var idade = prompt("Qual a sua idade?");
+// validaIdade(idade)
+// console.log(validar);
+
+// var d = new Date();
+// alert(d.getDate());
+// alert(d.getMonth() + 1);
+// alert(d.getDay());
+// alert(d.getHours());
+// alert(d.getMinutes());
+
+// var count;
+// for(count = 0; count <= 5; count++){
+//     alert(count);
+// } 
+
+// var count = 5;
+// while(count < 5){
+//     console.log(count);
+//     alert(count);
+//     count++;
+// }
+
+// var idade = prompt("Qual a sua idade?");
+// if(idade >= 18){
+//     alert("Maior de Idade!");
+// }else{
+//     alert("Menor de Idade!");
+// }
+
+
+// var frutas = [
+//     {nome:"maçã", cor:"vermelha"},
+//     {nome:"uva", cor:"roxa"}
+// ];
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+// var fruta = {nome:"maçã", cor:"vermelha"};
+
+// console.log(fruta);
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+// var lista = ["maçã", "pêra", "laranja"];
+// lista.push("uva");
+// ista.pop();
+
+// console.log(lista);
+// console.log(lista[1]);
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" | "));
+
+// var nome = "Carlos Alíco";
+// var idade = 20;
+// var idade2 = 10;
+// var n1 = 5;
+// var n2 = 3;
+// var frase = "Japão é o melhor time do mundo"
+// alert(nome + " tem " + idade + " anos");
+// alert(idade + idade2);
+// console.log(nome);
+// console.log(n1 + n2);
+// console.log(frase.replace("Japão", "Brasil"));
+// console.log(frase.toUpperCase(frase));
+//alert(frase.replace("Japão", "Brasil"));
+
+
+
+
+
+
+
